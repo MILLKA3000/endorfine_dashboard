b9ae315da7862a1b47a5ddd0e8fca9c5e54d744e
RMMusic <r.m.alien.ua@gmail.com>
2016-03-28 17:05:49 +0300

    footer add

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Client;
 use App\Http\Requests;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -24,6 +26,17 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $birthdays = Client::select('id', 'name', 'birthday')->get()->take(3);
+//        $thisDay = Carbon::parse("this day")->format('Y-m-d');
+//        select * from `client_info` where DATE_FORMAT(birthday,'%m-%d') >= DATE_FORMAT(NOW(),'%m-%d') and `client_info`.`deleted_at` is null
+//        $birthday = Client::where('DATE_FORMAT(FROM_UNIXTIME(birthday),\'%m-%d\')', '>', '\'DATE_FORMAT(NOW(),\'%m-%d\')\'')->get();
+
+        $datas = $birthdays;
+        $date = Carbon::parse("+1 day")->format('Y-m-d');
+        foreach ($datas as $value){
+            $value->enddate=$date;
+        }
+
+        return view('home', compact('birthdays', 'datas'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,65 +2,68 @@
 
 @section('content')
     <!-- Info boxes -->
-    <div class="row">
-        <div class="col-md-3 col-sm-6 col-xs-12">
-            <div class="info-box">
-                <span class="info-box-icon bg-aqua"><i class="ion ion-ios-gear-outline"></i></span>
+    {{--<div class="row">--}}
+        {{--<div class="col-md-3 col-sm-6 col-xs-12">--}}
+            {{--<div class="info-box">--}}
+                {{--<span class="info-box-icon bg-aqua"><i class="ion ion-ios-gear-outline"></i></span>--}}
 
-                <div class="info-box-content">
-                    <span class="info-box-text">CPU Traffic</span>
-                    <span class="info-box-number">90<small>%</small></span>
-                </div>
-                <!-- /.info-box-content -->
-            </div>
-            <!-- /.info-box -->
-        </div>
-        <!-- /.col -->
-        <div class="col-md-3 col-sm-6 col-xs-12">
-            <div class="info-box">
-                <span class="info-box-icon bg-red"><i class="fa fa-google-plus"></i></span>
+                {{--<div class="info-box-content">--}}
+                    {{--<span class="info-box-text">CPU Traffic</span>--}}
+                    {{--<span class="info-box-number">90<small>%</small></span>--}}
+                {{--</div>--}}
+                {{--<!-- /.info-box-content -->--}}
+            {{--</div>--}}
+            {{--<!-- /.info-box -->--}}
+        {{--</div>--}}
+        {{--<!-- /.col -->--}}
+        {{--<div class="col-md-3 col-sm-6 col-xs-12">--}}
+            {{--<div class="info-box">--}}
+                {{--<span class="info-box-icon bg-red"><i class="fa fa-google-plus"></i></span>--}}
 
-                <div class="info-box-content">
-                    <span class="info-box-text">Likes</span>
-                    <span class="info-box-number">41,410</span>
-                </div>
-                <!-- /.info-box-content -->
-            </div>
-            <!-- /.info-box -->
-        </div>
-        <!-- /.col -->
+                {{--<div class="info-box-content">--}}
+                    {{--<span class="info-box-text">Likes</span>--}}
+                    {{--<span class="info-box-number">41,410</span>--}}
+                {{--</div>--}}
+                {{--<!-- /.info-box-content -->--}}
+            {{--</div>--}}
+            {{--<!-- /.info-box -->--}}
+        {{--</div>--}}
+        {{--<!-- /.col -->--}}
 
-        <!-- fix for small devices only -->
-        <div class="clearfix visible-sm-block"></div>
+        {{--<!-- fix for small devices only -->--}}
+        {{--<div class="clearfix visible-sm-block"></div>--}}
 
-        <div class="col-md-3 col-sm-6 col-xs-12">
-            <div class="info-box">
-                <span class="info-box-icon bg-green"><i class="ion ion-ios-cart-outline"></i></span>
+        {{--<div class="col-md-3 col-sm-6 col-xs-12">--}}
+            {{--<div class="info-box">--}}
+                {{--<span class="info-box-icon bg-green"><i class="ion ion-ios-cart-outline"></i></span>--}}
 
-                <div class="info-box-content">
-                    <span class="info-box-text">Sales</span>
-                    <span class="info-box-number">760</span>
-                </div>
-                <!-- /.info-box-content -->
-            </div>
-            <!-- /.info-box -->
-        </div>
-        <!-- /.col -->
-        <div class="col-md-3 col-sm-6 col-xs-12">
-            <div class="info-box">
-                <span class="info-box-icon bg-yellow"><i class="ion ion-ios-people-outline"></i></span>
+                {{--<div class="info-box-content">--}}
+                    {{--<span class="info-box-text">Sales</span>--}}
+                    {{--<span class="info-box-number">760</span>--}}
+                {{--</div>--}}
+                {{--<!-- /.info-box-content -->--}}
+            {{--</div>--}}
+            {{--<!-- /.info-box -->--}}
+        {{--</div>--}}
+        {{--<!-- /.col -->--}}
+        {{--<div class="col-md-3 col-sm-6 col-xs-12">--}}
+            {{--<div class="info-box">--}}
+                {{--<span class="info-box-icon bg-yellow"><i class="ion ion-ios-people-outline"></i></span>--}}
 
-                <div class="info-box-content">
-                    <span class="info-box-text">New Members</span>
-                    <span class="info-box-number">2,000</span>
-                </div>
-                <!-- /.info-box-content -->
-            </div>
-            <!-- /.info-box -->
-        </div>
-        <!-- /.col -->
-    </div>
+                {{--<div class="info-box-content">--}}
+                    {{--<span class="info-box-text">New Members</span>--}}
+                    {{--<span class="info-box-number">2,000</span>--}}
+                {{--</div>--}}
+                {{--<!-- /.info-box-content -->--}}
+            {{--</div>--}}
+            {{--<!-- /.info-box -->--}}
+        {{--</div>--}}
+        {{--<!-- /.col -->--}}
+    {{--</div>--}}
     <!-- /.row -->
+    <div class="text-center text-warning">
+        <h1>Вас вітає Endirfine</h1>
+    </div>
     <div>
         <input class="form-control input-lg" type="text" placeholder="Ім'я клієнта або номер абонимента">
     </div>
@@ -76,7 +79,32 @@
     <div class="row">
         <div class="col-md-12 col-sm-12 col-xs-12">
             <div class="box box-warning">
-        Футер
+                <div class="col-md-6 col-sm-6 col-xs-12">
+                    <div class="box box-widget widget-user-2 widget-user-header dashboard_footer">
+                        <!-- Add the bg color to the header using any of the bg-* classes -->
+                            <div class="widget-user-image">
+                                <img class="img-circle" src="/img/birthday-cake1.jpg" alt="User Avatar">
+                            </div>
+                            <!-- /.widget-user-image -->
+                            <a href="/clients/id">
+                            @foreach ($birthdays as $birthday)
+                            <a href="clients/{{$birthday->id}}"><h5 class="widget-user-desc">{{$birthday->birthday}} - {{$birthday->name}}</h5></a>
+                            @endforeach
+                    </div>
+                </div>
+                <div class="col-md-6 col-sm-6 col-xs-12">
+                    <div class="box box-widget widget-user-2 widget-user-header dashboard_footer">
+                        <!-- Add the bg color to the header using any of the bg-* classes -->
+                        <div class="widget-user-image">
+                            <img class="img-circle" src="/img/ticket-icon_1.png" alt="User Avatar">
+                        </div>
+                        <!-- /.widget-user-image -->
+                        <a href="/clients/id">
+                            @foreach ($datas as $data)
+                                <a href="clients/{{$birthday->id}}"><h5 class="widget-user-desc">{{$data->enddate}} - {{$data->name}}</h5></a>
+                        @endforeach
+                    </div>
+                </div>
             </div>
         </div>
     </div>
