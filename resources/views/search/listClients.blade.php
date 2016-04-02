8b4bac9ee04509d714fb354fb8d481635d49c80e
demoffon <dmitroch7@gmail.com>
2016-04-02 13:26:53 +0300

    add photo to search listClients.blade.php

--- resources/views/search/listClients.blade.php
+++ resources/views/search/listClients.blade.php
@@ -1,10 +1,13 @@
 <div>
     <div class="col-md-12 col-sm-12 col-xs-12 responsive">
-        <div class="info-box bg-yellow">
-            <span class="info-box-icon"><i class="fa fa-calendar"></i></span>
+        <div class="info-box bg-warning">
+            {{--<div class="box-body box-profile"> <img class="img-responsive" style="width: 100%" src="{{$numAbonement->getNameClient->photo}}" alt="Фото клієнта"></div>--}}
 
             <div class="info-box-content">
-                <div class="col-md-3">
+                <div class="col-md-1">
+                    <img class="img-responsive" style="width: 100%" src="{{$clients->photo}}" alt="Фото клієнта">
+                </div>
+                <div class="col-md-4">
                     <span class="info-box-text">Ім'я</span>
                     <span class="info-box-text">{{$clients->name}}</span>
                 </div>
@@ -12,17 +15,17 @@
                     <span class="info-box-text">День нардження</span>
                     <span class="info-box-text">{{$clients->birthday}}</span>
                 </div>
-                <div class="col-md-3">
+                <div class="col-md-4">
                     <span class="info-box-text">Телефон</span>
                     <span class="info-box-text">{{$clients->phone}}</span>
                 </div>
                 <div class="col-md-3">
-                <div class="progress">
-                    <div class="progress-bar" style="width: 70%"></div>
-                </div>
-                  <span class="progress-description">
-                    70% Increase in 30 Days
-                  </span>
+                    <div class="progress">
+                        <div class="progress-bar" style="width: 70%"></div>
+                    </div>
+                    <span class="progress-description">
+                            70% Increase in 30 Days
+                    </span>
                 </div>
             </div>
             <!-- /.info-box-content -->
