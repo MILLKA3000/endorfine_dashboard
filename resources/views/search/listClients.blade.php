47520120dc9f4d7c86cc9e5064a1d41e6d49422f
demoffon <dmitroch7@gmail.com>
2016-04-04 12:15:57 +0300

    listClients.blade.php 1

--- resources/views/search/listClients.blade.php
+++ resources/views/search/listClients.blade.php
@@ -1,17 +1,20 @@
 <a href="#" class="click-to-detail" data-id="{{$clients->getNumTicket->numTicket}}">
     <div class="col-md-12 col-sm-12 col-xs-12 responsive">
-        <div class="info-box bg-warning">
+        <div class="info-box search-list-item">
             {{--<div class="box-body box-profile"> <img class="img-responsive" style="width: 100%" src="{{$numAbonement->getNameClient->photo}}" alt="Фото клієнта"></div>--}}
 
-            <div class="info-box-content">
+            <div class="info-box-content search-list-item">
                 <div class="col-md-1">
-                    <img class="img-responsive" style="width: 100%" src="{{$clients->photo}}" alt="Фото клієнта">
+                    <img class="img-responsive" src="{{$clients->photo}}" alt="Фото клієнта">
+                    @if( $clients->birthday == date('m-d') )
+                        <img class="img-responsive" style="position: absolute; top: -20px; left: -20px; width: 40%" src="/img/birthday-surp.png">
+                    @endif
                 </div>
-                <div class="col-md-4">
+                <div class="col-md-4 search-list-item">
                     <span class="info-box-text">Ім'я</span>
                     <span class="info-box-text">{{$clients->name}}</span>
                 </div>
-                <div class="col-md-3">
+                <div class="col-md-3 search-list-item">
                     <span class="info-box-text">День нардження</span>
                     <span class="info-box-text">{{$clients->birthday}}</span>
                 </div>
