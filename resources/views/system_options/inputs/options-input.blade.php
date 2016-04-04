77a327a6800efe88dc7034df26d7a5ada525fb95
RMMusic <r.m.alien.ua@gmail.com>
2016-04-04 18:04:02 +0300

    111

--- resources/views/system_options/inputs/options-input.blade.php
+++ resources/views/system_options/inputs/options-input.blade.php
@@ -1,5 +1,5 @@
 <div class="box-header">
     <label><h3 class="box-title">{{$option->name}}</h3></label>
-    <input class="form-control input-lg" type="text" name="{{$option->key}}" placeholder="{{$option->value}}">
+    <input class="form-control input-lg" type="text" name="{{$option->key}}" value="{{$option->value}}" placeholder="{{$option->value}}">
     ____________________________________________
 </div>
